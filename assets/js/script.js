6f10ae420fcd84ea7516667f1a43f6468b6aa408
MariuszKaminski <mkaminski10@gmail.com>
2021-10-13 23:04:11 +0000

    add function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-console.log("connected")
+function rungGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function checkCorrectAnswer() {
+
+}
+
+function incrementCorrectAnswer() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayCapitalsQuestion() {
+
+}
+
+function displayRiversQuestion() {
+
+}
+
+function displayMountainsQuestion() {
+
+}
+
+function displayContinentsQuestion() {
+
+}
+
+
