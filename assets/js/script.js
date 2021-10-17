b8c9ef9e5c397d0b83d6801d8ebfe0c972990abb
MariuszKaminski <mkaminski10@gmail.com>
2021-10-17 16:23:54 +0000

    Add button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function rungGame() {
+
+
+
+document.addEventListener("DOMConentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked $(gameType)`);
+            }
+        })
+    }
+})
+
+function runGame() {
 
 }
 
