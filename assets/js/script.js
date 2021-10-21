ee2e4149a9805ceffe090d625e9b7b30fb613149
MariuszKaminski <mkaminski10@gmail.com>
2021-10-21 21:23:06 +0000

    Add generate answers code

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+
+
 function runGame(gameType) {
 
     let random_object = Math.floor(Math.random() * 4);
@@ -51,11 +53,22 @@ function runGame(gameType) {
     if (gameType === "capitals") {
         displayCapitalsQuestion(countryName);
     } else {
-        alert('Unknown game type: $(gameType)');
-        throw 'Unknown game type: ${gameType). Aborting!';
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
+
+function getCapitals(input, field) {
+    let output = [];
+    for (var i=0; i < input.length; ++i)
+        output.push(input[i][field]);
+    return output;
+}
+
+let capitalsArr = getCapitals(countries, "name");
+//let capitalsSub = capitalsArr.slice(0, 2);
+//let capitalsAddCorr = capitalsSub.push(countryName);
 
 function checkAnswer() {
 
@@ -76,6 +89,9 @@ function incrementWrongAnswer() {
 function displayCapitalsQuestion(countryName) {
     document.getElementById('country-name').textContent = countryName;
 }
+
+function displayCapitalsAnswers()
+
 
 function displayRiversQuestion() {
 
